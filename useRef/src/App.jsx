713dd7619a3177ac6dd68b2743e46206af7d0b24
Aurelily <aurelie.preaud@gmail.com>
2023-10-27 15:54:32 +0200

    Ajout exemple useRef

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -1,40 +1,24 @@
-import { useRef } from 'react'
+import React from 'react'
 import './App.css'
-import { useEffect } from 'react';
+
+
+import Demo01 from './components/Demo01'
+import Demo02 from './components/Demo02'
+import Demo03 from './components/Demo03'
+import Demo04 from './components/Demo04'
+import Demo05 from './components/Demo05'
+import Demo06 from './components/Demo06'
+import Demo07 from './components/Demo07'
 
 function App() {
 
-  const inputRef = useRef(0)
-
-  const isButtonClicked = useRef(false)
-
-  console.log(inputRef);
-
-  const handleClick = () =>{
-    if(isButtonClicked.current){
-      console.log("already clicked");
-      return;
-    }
-    console.log("Clicked");
-    isButtonClicked.current = true;
-  }
-
-  useEffect(()=>{
-    inputRef.current.focus()
-    inputRef.current.style.background = "red"
-  }, [])
+ 
 
   return (
     <>
-      <input type="text" ref={inputRef}/>
 
-      {/* ref peut aussi etre une fonction callback concernant l'élément sur lequel il se trouve afin de le manipuler à son premier render. Pas besoin d'un useEffect*/}
-      <input type="text" ref={(input)=>{
-        console.log(input);
-        input.style.background = "green"
-      }}/>
-
-      <button onClick={handleClick}>Cliquez moi</button>
+    <Demo07/>
+  
     </>
   )
 }
